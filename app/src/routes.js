a25928a5551f9f68dcd66de6ee02e5f2cc751bb2
mokupsas <mockus.ri@gmail.com>
2023-03-20 01:31:58 +0200

    created routes.js route config

---
+++ app/src/routes.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+// Views
+import Login from './views/Login.vue';
+
+Vue.use(VueRouter);
+
+let routes = [
+    {
+        path: '/',
+        name: 'Login',
+        component: Login
+    }
+];
+
+export default new VueRouter({
+    //mode: 'history',
+    routes
+});
