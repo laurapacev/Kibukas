25a33ce48bf2580a68c1c3e219e32cd3ba6c7dab
mokupsas <mockus.ri@gmail.com>
2023-03-20 01:52:14 +0200

    updated Navigation with header elements

--- app/src/components/Navigation.vue
+++ app/src/components/Navigation.vue
@@ -1,5 +1,11 @@
 <template>
-  
+  <nav class="navbar bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        Kibukas
+      </a>
+    </div>
+  </nav>
 </template>
 
 <script></script>
