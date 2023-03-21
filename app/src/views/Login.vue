11ea638bf5665f4eeb79c8fadb55ec66af4d7388
mokupsas <mockus.ri@gmail.com>
2023-03-21 10:36:43 +0200

    updated Login

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,15 +1,47 @@
 <template>
-  <p>test</p>
+  <div class="container-login">
+    <button v-if="!isUserLoggedIn()" class="sign-in">Prisijungti su Google paskyra</button>
+    <p v-else>Prisijungta</p>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      
+      user: []
+    }
+  },
+  methods: {
+    isUserLoggedIn() {
+      if (this.user.length > 0 )
+        return true
+      return false
     }
   }
 }
 </script>
 
-<style></style>
+<style>
+.container-login {
+  margin-top: 50px;
+}
+.sign-in {
+  display: block;
+  color: #282c34;
+  background: white;
+  max-width: 400px;
+  margin: 0 auto;
+}
+button {
+  background-color: #282c34;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  cursor: pointer;
+  font-size: 1.25rem;
+}
+</style>
