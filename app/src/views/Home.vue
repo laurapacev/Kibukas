852ec1a7585cda7995704870e17aed657f274176
mokupsas <mockus.ri@gmail.com>
2023-03-22 09:57:37 +0200

    created Home component

---
+++ app/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <p>Home</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style></style>
