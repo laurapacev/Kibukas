83b8a7408fbbabb08583b824e56af6d4d6415655
mokupsas <mockus.ri@gmail.com>
2023-03-22 09:32:32 +0200

    updated all components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Routes
+import router from './routes'
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
