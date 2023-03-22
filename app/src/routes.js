f32ef344cdc752fce5d9c353d6ef022ab36f2eb5
mokupsas <mockus.ri@gmail.com>
2023-03-22 09:57:25 +0200

    created Home route

--- app/src/routes.js
+++ app/src/routes.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 // Views
+import Home from './views/Home.vue';
 import Login from './views/Login.vue';
 
 Vue.use(VueRouter);
@@ -9,6 +10,11 @@ Vue.use(VueRouter);
 let routes = [
     {
         path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/login',
         name: 'Login',
         component: Login
     }
