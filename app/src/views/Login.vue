360cfaaf7acdf5223ba0856ebc70b71037296919
mokupsas <mockus.ri@gmail.com>
2023-03-25 10:10:25 +0200

    updated Login to work with user store

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,22 +1,61 @@
 <template>
   <div class="container-login">
-    <button v-if="!isUserLoggedIn()" @click="signInWithGoogle()"  class="sign-in">Sign In</button>
+    <button v-if="!this.$store.getters.isUserLoggedIn" @click="signInWithGoogle()"  class="sign-in">Sign In</button>
     <button @click="test()">test</button>
   </div>
 </template>
 
 <script>
+// Firebase
+import router from "@/routes";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAD1WEV3K283NKoCXsRxaBk--KV9piKpCc",
+  authDomain: "kibukas-68495.firebaseapp.com",
+  projectId: "kibukas-68495",
+  storageBucket: "kibukas-68495.appspot.com",
+  messagingSenderId: "718635856365",
+  appId: "1:718635856365:web:1e462bc10bf8fcd19277e4",
+  measurementId: "G-H087EJPNHJ"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(firebaseApp);
+const provider = new GoogleAuthProvider();
+
 export default {
-  data () {
-    return {
-      user: []
-    }
-  },
   methods: {
-    isUserLoggedIn() {
-      if (this.user.length > 0 )
-        return true
-      return false
+    signInWithGoogle()
+    {
+      signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user)
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+    },
+    created()
+    {
+      console.log("login")
     }
   }
 }
