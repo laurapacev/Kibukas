58a1dd9fa157554642321c173417ce26f967da14
mokupsas <mockus.ri@gmail.com>
2023-03-25 10:39:27 +0200

    updated App

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,39 +3,19 @@
     <navigation></navigation>
     <div class="container">
 
-      <router-view v-if="apiLoaded == true"></router-view>
-      <div v-else class="spinner-border load-spinner" role="status"></div>
+      <router-view></router-view>
 
     </div>
   </div>
 </template>
 
 <script>
-// Stores
 // Componets
 import Navigation from './components/Navigation.vue'
 
 export default {
   components: { Navigation },
-  name: 'App',
-  data() {
-    return {
-      apiLoaded: false
-    }
-  },
-  computed: {
-    getApiLoadChange() {
-      return this. $store.state.user.apiState
-    }
-  },
-  watch: {
-    getApiLoadChange(newState, oldState) {
-      if(newState == 1)
-      {
-        this.apiLoaded = true
-      }
-    }
-  }
+  name: 'App'
 }
 </script>
 
@@ -45,14 +25,5 @@ html, body {
   width: 100%;
   background-color: rgb(40, 37, 53);
   color: #ffffff;
-}
-.load-spinner {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  margin-top: -4em; /* Negative half of height. */
-  margin-left: -4em; /* Negative half of width. */
-  width: 4rem;
-  height: 4rem;
 }
 </style>
