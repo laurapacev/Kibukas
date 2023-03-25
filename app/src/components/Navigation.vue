488948ec6dff83d9c76e725bbbb1d4223bdd312f
mokupsas <mockus.ri@gmail.com>
2023-03-25 11:07:44 +0200

    added logout button

--- app/src/components/Navigation.vue
+++ app/src/components/Navigation.vue
@@ -1,14 +1,31 @@
 <template>
   <nav class="header navbar navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <b>KIBUKAS</b>
-      </a>
+    <div class="container d-flex flex-row">
+        <div class="">
+          <a class="navbar-brand" href="#">
+            <b>KIBUKAS</b>
+          </a>
+        </div>
+        <div class="">
+          <button v-if="this.$store.getters.isUserLoggedIn" @click="logout()">Logout</button>
+        </div>
     </div>
   </nav>
 </template>
 
-<script></script>
+<script>
+// Mixins
+import { Firebase } from '../mixins/Firebase'
+
+export default {
+  mixins: [ Firebase ],
+  methods: {
+    logout() {
+      console.log(this.getAuth().signOut())
+    }
+  }
+}
+</script>
 
 <style>
 .header {
