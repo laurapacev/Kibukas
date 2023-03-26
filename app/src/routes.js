c6cd8c59e0a368082f0a2fa570b5007276fc50f4
mokupsas <mockus.ri@gmail.com>
2023-03-26 21:17:31 +0300

    added Register view to routes

--- app/src/routes.js
+++ app/src/routes.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router';
 // Views
 import Home from './views/Home.vue';
 import Login from './views/Login.vue';
+import Register from './views/Register.vue';
 
 Vue.use(VueRouter);
 
@@ -16,7 +17,16 @@ let routes = [
             requiresAuth: false,
             requiresNoAuth: true
         }
-    },    
+    }, 
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register,
+        meta: {
+            requiresAuth: false,
+            requiresNoAuth: true
+        }
+    },  
     {
         path: '/',
         name: 'Home',
