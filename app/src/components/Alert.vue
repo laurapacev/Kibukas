98c5fb2a4aa7a503ab33ab29bf6ffca994b1416a
mokupsas <mockus.ri@gmail.com>
2023-03-31 10:57:04 +0300

    created Alert component

---
+++ app/src/components/Alert.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="alert" :class="getType()" role="alert">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: { default: 'warning' }
+  },
+  data() {
+    return {
+      types: [
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'info',
+        'light',
+        'dark'
+      ]
+    }
+  },
+  methods: {
+    getType() {
+      let type = this.type
+      if(type == 'error') type = 'danger'
+
+      let element = this.types.find(element => element == type)
+
+      if(element)
+        return 'alert-' + element
+
+      return 'alert-primary'
+    }
+  },
+  created() {
+    console.log(this.getType())
+  }
+}
+</script>
+
+<style></style>
