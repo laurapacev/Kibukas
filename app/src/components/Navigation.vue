17d0abdc95890c14f24328e10157271dcc6e32fa
mokupsas <mockus.ri@gmail.com>
2023-03-31 11:29:46 +0300

    added Login link to Navigation

--- app/src/components/Navigation.vue
+++ app/src/components/Navigation.vue
@@ -4,7 +4,10 @@
           <a class="navbar-brand" href="#">
             <b>KIBUKAS</b>
           </a>
-          <button v-show="this.$store.getters.isUserLoggedIn == true" @click="logout()">Logout</button>
+          <div>
+            <router-link class="nav-btn" to="/login">Login</router-link>
+            <button v-show="this.$store.getters.isUserLoggedIn == true" @click="logout()">Logout</button>
+          </div>
     </div>
   </nav>
 </template>
@@ -32,5 +35,18 @@ export default {
 .header {
   background-color: rgba(24, 23, 23, 1) !important;
   color: #ffffff !important;
+}
+.nav-btn {
+  display: block;
+  background: white;
+  border: none;
+  color: #282c34;
+  padding: 4px 10px;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  font-size: 1.25rem;
+  max-width: 400px;
+  margin: 0 auto;
 }
 </style>
