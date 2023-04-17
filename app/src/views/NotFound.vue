9b485b93d857662e7c0c7d669388ce35aaeae86c
Shiuw <r.mantas2002@gmail.com>
2023-04-18 00:08:54 +0300

    added page not found
    idejau pagenotfound su route

---
+++ app/src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="not-found">
+    <h1 class = "pag">Page not found</h1>
+    <p>Sorry, the page you're looking for doesn't exist.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+<style>
+.not-found {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.pag{
+  color: red;
+}
+</style>
