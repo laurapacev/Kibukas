8e7eff00a4fe9644ed0ccd860ca3a84cfcbd8446
mokupsas <mockus.ri@gmail.com>
2023-04-28 00:46:22 +0300

    created RecentChatsItem

---
+++ app/src/components/chat/RecentChatsItem.vue
@@ -0,0 +1,74 @@
+<template>
+  <a href="#" class="list-group-item recent-chat recent-read" aria-current="true">
+    <div class="row">
+
+      <div class="col-md-auto">
+        <i class="fa-solid fa-circle-user recent-profile"></i>
+      </div>
+
+      <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">{{ name }}</h5>
+          <!-- <span class="badge bg-primary rounded-pill">14</span >-->
+          <small>{{ getTimeAgo() }}</small>
+        </div>
+        <div class="d-flex w-100 justify-content-between">
+          <p class="mb-1 recent-message"><slot></slot></p>
+        </div>
+      </div>
+
+    </div>
+  </a>
+
+</template>
+
+<script>
+export default {
+  props: {
+    name: { required: true }
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    getTimeAgo() {
+      return '3 days'
+    }
+  }
+}
+</script>
+
+<style>
+.list-group-item {
+  border: none;
+}
+
+.recent-chat {
+  background-color: transparent;
+}
+
+.recent-chat:hover {
+  color: #c8bcff;
+}
+
+.recent-read {
+  color: #bcbcbc;
+  border-left: rgba(255, 255, 255, 0) solid 5px;
+}
+.recent-unread {
+  color: #fff;
+  border-left: rgb(63, 55, 100) solid 5px;
+}
+
+.recent-profile {
+  line-height: 55px;
+  font-size: 36px;
+}
+
+.recent-message {
+  filter: brightness(85%);
+  opacity: 0.5;
+}
+</style>
