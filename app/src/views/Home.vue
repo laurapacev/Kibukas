c1b329239c6840b6f9d3f786a70997c09a1495d4
mokupsas <mockus.ri@gmail.com>
2023-04-28 00:47:02 +0300

    updated Home with new components

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,11 +1,27 @@
 <template>
+
   <div class="container">
-    <p>Home</p>
+    <div class="row">
+      <div class="col-4 chat-list-column ">
+        <ChatsHeader></ChatsHeader>
+        <RecentChats></RecentChats>
+      </div>
+      <div class="col chat-column">
+        <ChatBox></ChatBox>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
+// Componets
+import ChatsHeader from '../components/chat/ChatsHeader.vue'
+import RecentChats from '../components/chat/RecentChats'
+import ChatBox from '../components/chat/ChatBox'
+
 export default {
+  components: { RecentChats, ChatsHeader, ChatBox },
   data () {
     return {
 
@@ -17,4 +33,11 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.chat-list-column {
+  min-width: 320px;
+}
+.chat-column {
+  background-color: linear-gradient(#ffffff, #1f1d2b) !important;
+}
+</style>
