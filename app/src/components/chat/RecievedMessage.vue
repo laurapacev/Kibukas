7dee1c34e336f5d12c3fe27723250c35e047239e
mokupsas <mockus.ri@gmail.com>
2023-04-28 00:46:47 +0300

    created RecievedMessage for chat box

---
+++ app/src/components/chat/RecievedMessage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+    <div class="dm-message dm-recieved mb-2">
+      <slot></slot>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+.dm-recieved {
+  background-color: #dededf !important;
+  color: black;
+}
+</style>
