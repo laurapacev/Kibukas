37387a7542f72dd7897aa95faa9d9afbe40edfac
mokupsas <mockus.ri@gmail.com>
2023-04-28 00:46:10 +0300

    created RecentChats list

---
+++ app/src/components/chat/RecentChats.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="list-group recent-chats-container overflow-auto">
+      
+      <div v-for="recent in array">
+        <RecentChatsItem :name="recent">{{ recent }}</RecentChatsItem>
+      </div>
+
+    </ul>
+</template>
+
+<script>
+// Componets
+import RecentChatsItem from './RecentChatsItem'
+
+export default {
+  components: { RecentChatsItem },
+  data() {
+    return {
+      array: ['Vartotojas', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b']
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+.recent-chats-container {
+  height: calc(100vh - 150px);
+  scrollbar-color: #6e6691 #0000;
+  scrollbar-width: thin;
+}
+</style>
