6f4eb940d28796e30cff5d28b7905bd11be055e1
mokupsas <mockus.ri@gmail.com>
2023-04-28 00:46:37 +0300

    created SentMessage for chat box

---
+++ app/src/components/chat/SentMessage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+    <div class="dm-message dm-my mb-2">
+      <slot></slot>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+
+.dm-my {
+  background-color: RGBA(13,110,253,1) !important;
+  float: right;
+}
+
+</style>
