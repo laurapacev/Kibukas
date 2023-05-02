92e1b338743c778c6b551b90281560996fabe5ac
laurapaceviciute <99803394+laurapaceviciute@users.noreply.github.com>
2023-05-02 07:55:02 +0300

    Login Database

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -23,6 +23,11 @@
 <script>
 import router from "@/routes";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getDatabase, set, ref, update } from "firebase/database";
+
+//Initialize firebase
+const auth = getAuth();
+const database = getDatabase();
 // Componets
 import Alert from '../components/Alert.vue'
 
@@ -72,7 +77,19 @@ export default {
       {
         return
       }
+      signInWithEmailAndPassword(auth, email, password) .then((userCredential) => {
+      const user = userCredential.user;
 
+      const dt = new Date();
+      update(ref(database, 'users/' + user.uid),{
+        last_login:dt,
+        email:email
+      })
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
       this.showAlert(true, 'Login data was successfully supplied', 'success')
     },
     checkInput()
