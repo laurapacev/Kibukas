82513072d2bd4f798c9b0fe7a76f5fc3a882308b
mokupsas <mockus.ri@gmail.com>
2023-05-10 01:05:39 +0300

    updated ChatBox to send and show messages

--- app/src/components/chat/ChatBox.vue
+++ app/src/components/chat/ChatBox.vue
@@ -3,33 +3,14 @@
 
     <div class="chat-box chat-scrollbar overflow-auto">
 
-      <SentMessage>
-        Suspendisse at velit quis tellus pellentesque imperdiet in sit amet purus. Aenean auctor quam sit amet mi dictum, in eleifend magna consectetur. Praesent iaculis placerat mauris, nec vestibulum augue placerat at. Fusce in pretium tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi varius felis non arcu congue, sollicitudin consequat neque interdum. Vestibulum mollis ultrices nulla, vitae mollis sem gravida vel. 
-      </SentMessage>
-
-      <RecievedMessage>
-        Suspendisse at velit quis tellus pellentesque imperdiet in sit amet purus. Aenean auctor quam sit amet mi dictum, in eleifend magna consectetur. Praesent iaculis placerat mauris, nec vestibulum augue placerat at. Fusce in pretium tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi varius felis non arcu congue, sollicitudin consequat neque interdum. Vestibulum mollis ultrices nulla, vitae mollis sem gravida vel. 
-      </RecievedMessage>
-
-      <SentMessage>
-        Suspendisse at velit quis tellus pellentesque imperdiet in sit amet purus. Aenean auctor quam sit amet mi dictum, in eleifend magna consectetur. Praesent iaculis placerat mauris, nec vestibulum augue placerat at. Fusce in pretium tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi varius felis non arcu congue, sollicitudin consequat neque interdum. Vestibulum mollis ultrices nulla, vitae mollis sem gravida vel. 
-      </SentMessage>
-
-      <RecievedMessage>
-        Suspendisse at velit quis tellus pellentesque imperdiet in sit amet purus. Aenean auctor quam sit amet mi dictum, in eleifend magna consectetur. Praesent iaculis placerat mauris, nec vestibulum augue placerat at. Fusce in pretium tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi varius felis non arcu congue, sollicitudin consequat neque interdum. Vestibulum mollis ultrices nulla, vitae mollis sem gravida vel. 
-      </RecievedMessage>
-
-      <SentMessage>
-        Suspendisse at velit quis tellus pellentesque imperdiet in sit amet purus. Aenean auctor quam sit amet mi dictum, in eleifend magna consectetur. Praesent iaculis placerat mauris, nec vestibulum augue placerat at. Fusce in pretium tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi varius felis non arcu congue, sollicitudin consequat neque interdum. Vestibulum mollis ultrices nulla, vitae mollis sem gravida vel. 
-      </SentMessage>
-
-      <RecievedMessage>
-        Suspendisse at velit quis tellus pellentesque imperdiet in sit amet purus. Aenean auctor quam sit amet mi dictum, in eleifend magna consectetur. Praesent iaculis placerat mauris, nec vestibulum augue placerat at. Fusce in pretium tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi varius felis non arcu congue, sollicitudin consequat neque interdum. Vestibulum mollis ultrices nulla, vitae mollis sem gravida vel. 
-      </RecievedMessage>
+      <div v-for="message in messages">
+        <SentMessage v-if="message.status == 'sent'">{{ message.message }}</SentMessage>
+        <RecievedMessage v-else>{{ message.message }}</RecievedMessage>
+      </div>
 
     </div>
 
-    <ChatInput @message="addMessage"></ChatInput>
+    <ChatInput @message="sendMessage"></ChatInput>
 
   </div>
   <div v-else class="no-chat-container">
@@ -44,26 +25,87 @@ import ChatInput from './ChatInput.vue'
 
 import { Firebase } from '../../mixins/Firebase'
 
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+
 export default {
   components: { SentMessage, RecievedMessage, ChatInput },
   mixins: [ Firebase ],
   props: {
     recipientUid: { default: null }
   },
+  watch: { 
+    // When user changes with who to chat, recipientUid is is changed
+    // so we load chats  
+    recipientUid: async function(new_recipient_uid, old_recipient_uid) 
+    {
+      this.subscribeNewMessage(new_recipient_uid)
+    }
+  },
+  data() {
+    return {
+      user_uid: this.$store.getters.getUser.uid,
+      messages: []
+    }
+  },
   methods: {
-    async addMessage(text)
+    async getUserMessagesArray() {
+      const user_uid = this.$store.getters.getUser.uid
+      let messages;
+
+      let sent_msgs = await this.getSentMessages(user_uid, this.recipientUid)
+      let recieved_msgs = await this.getRecievedMessages(user_uid, this.recipientUid)
+
+      // Merge sent and recieved messages into one array
+      messages = sent_msgs.concat(recieved_msgs)
+
+      // Sort messages by timestamp (newest messages at the top)
+      messages.sort((a,b) => b.timestamp - a.timestamp); // b - a for reverse sort
+
+
+      return messages
+    },
+    mergeArray(to, from)
+    {
+      from.forEach((item) => to.push(item))
+      return to
+    },
+    // Get user sent messages
+    async getSentMessages(user_uid, recipient_uid)
+    {
+      let sentArr = await this.getDocumentsTwoWhere('messages', 'from', '==', user_uid, 'to', '==', recipient_uid)
+      if(sentArr == false ) return []
+
+      sentArr.map(obj => { obj.status = 'sent'})  // adds status, that message was sent
+
+      return sentArr
+    },
+    // Get user recieved messages
+    async getRecievedMessages(user_uid, sender_uid)
+    {
+      let recievedArr = await this.getDocumentsTwoWhere('messages', 'to', '==', user_uid, 'from', '==', sender_uid)
+      if(recievedArr == false ) return []
+
+      recievedArr.map(obj => { obj.status = 'recieved' }) // adds status, that message was recieved
+
+      return recievedArr
+    },
+    async sendMessage(text)
     {
       if(!text) return
 
       this.setDocument('messages', {
         from: this.$store.getters.getUser.uid,
         to: this.recipientUid,
-        message: text
+        message: text,
+        timestamp: new Date().getTime()
       })
     },
-    changeReciepient(uid)
+    subscribeNewMessage(recipient_uid)
     {
-      this.recipientUid = uid
+      const q = query(collection(this.getDb(), "messages"), where("from", "==", this.user_uid), where('to', '==', recipient_uid));
+      const unsubscribe = onSnapshot(q, async (querySnapshot) => {
+        this.messages = await this.getUserMessagesArray()
+      });
     }
   }
 }
