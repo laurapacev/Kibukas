b43aca9c3a2c5372e872a4ee98c421fb4ff7d63b
mokupsas <mockus.ri@gmail.com>
2023-05-11 01:42:57 +0300

    created MessageDropdown

---
+++ app/src/components/chat/MessageDropdown.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+
+</template>
+
+<script>
+export default {
+  props: { 
+    docId: { required: true }
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+
+  },
+  created()
+  {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  }
+}
+</script>
+
+<style></style>
